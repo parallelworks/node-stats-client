5933b65cc6b01105262a200243f8a48ccde672dd
Michael McQuade <michael@giraffesyo.io>
2022-08-18 11:26:56 -0500

    Add in slurm checker

---
+++ getSlurm.sh
@@ -0,0 +1,45 @@
+
+#!/bin/bash
+
+START=`date +%Y-%m-%dT%H:%M:%S -d"-60 sec"`
+
+JOBS=`/usr/bin/sacct -n -a -L -S $START --format=Cluster%64,User,UID,ElapsedRaw,JobID,Submit,Start,End,JobName,AllocNodes,AllocCPUS,State -P`
+
+BIFS="$IFS"
+IFS=$'\n'
+
+##Generate query file
+
+for job in $JOBS
+do
+
+echo "$job"
+IFS='|' read Cluster UserName UserID ElapsedRaw JobID Submit StartTime EndTime JobName AllocNodes AllocCPUS State  <<< "$job"
+
+read -r -d '' data << EOM
+{
+    "cluster":"$Cluster",
+    "poolname":"$PW_POOL_NAME",
+    "session":"$PW_SESSION",
+    "user":"$UserName",
+    "project":"$PW_GROUP",
+    "job_name":"$JobName",
+    "uid":"$UserID",
+    "elapsed_sec":"$ElapsedRaw",
+    "job_id":"$JobID",
+    "submit_time":"$Submit",
+    "start_time":"$StartTime",
+    "end_time":"$EndTime",
+    "nodes":"$AllocNodes",
+    "cpus":"$AllocCPUS",
+    "state":"$State"
+}
+EOM
+
+# REST API POST REQUEST
+echo
+echo $data
+
+curl -X POST -H "Content-Type: application/json" -d "$data" https://$PW_PLATFORM_HOST/api/v2/stats/slurm
+
+done
